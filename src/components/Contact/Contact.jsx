3c3a13838b21186796d4a9cc2464954ca919e5d7
Abuckard <alexander.buckard@gmail.com>
2024-05-03 09:51:06 +0200

    Fungerande API

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import Header from '../Header/Header'
+import Header from "../Header/Header"
 
 export default function Contact() {
-  return (
-    <div>
-        <Header />
-        <div>Contact</div>
-    </div>
-  )
-}
+    return (
+      <div>
+          <Header />
+          <div>Contact</div>
+      </div>
+    )
+  }
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
-import Header from '../Header/Header'
+import React, { useState, useEffect } from 'react'; // Här importerar jag useState och useEffect
+import Header from '../Header/Header'; // Importerar min Header-komponent som används på alla sidor.
+
+
+//Här under kallar jag på GitHub API. Med hjälp av async och await säkrar jag upp att inte sidan laddas innan all information är hämtad.
+async function getGitHubUserInfo(username) {
+    try {
+        const userResponse = await fetch(`https://api.github.com/users/${username}`);
+        const userData = await userResponse.json();
+        const followers = userData.followers;
+
+        const reposResponse = await fetch(userData.repos_url);
+        const reposData = await reposResponse.json();
+        const reposCount = reposData.length;
+        const userLogin = userData.login;
+
+        return { followers, reposCount,userLogin };
+    } catch (error) {
+        console.error("Error fetching GitHub user info:", error); // Skriver ut felmeddelandet i konsolen om något fel inträffar
+        return null;
+    }
+}
 
 export default function Project() {
-  return (
-    <div>
-        <Header />
-        <div>Project</div>
-    </div>
-  )
+    const [githubInfo, setGithubInfo] = useState(null); // Här sätter jag githubInfo till null i starten av sidan, sedan hämtar den ett objekt och uppdaterar det med hjälp av setGithubInfo.
+
+    useEffect(() => {
+        const username = "Abuckard"; 
+        if (username) {
+            getGitHubUserInfo(username)
+                .then((data) => {
+                    setGithubInfo(data);
+                })
+                .catch(error => console.error("Error:", error));
+        }
+    }, []); 
+
+    return ( // Här skriver jag sen ut informationen jag hämtat från APIn på sidan.
+        <div>
+            <Header />
+            <div>Project</div>
+            {githubInfo && (
+                <div>
+                    <p>Användare: {githubInfo.userLogin}</p>
+                    <p>Antal följare: {githubInfo.followers}</p>
+                    <p>Antal repositories: {githubInfo.reposCount}</p>
+
+                </div>
+            )}
+        </div>
+    );
 }
